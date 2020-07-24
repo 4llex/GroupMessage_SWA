68ec804b32045515cfddc6f3bda27b240061e236
Alex Rosa <alexr@gec.inatel.br>
2020-07-24 14:08:47 -0300

    primeiro commit - inicializando projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GroupMessageApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
